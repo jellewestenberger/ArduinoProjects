1bd02ff1ee37c13ba7cbf25cbbfdf362cd57791e
Jelle Westenberger <jellewestenberger@gmail.com>
2020-10-05 00:17:42 +0200

    added light sensor readings

--- mqttTemperature/mqttTemperature.ino
+++ mqttTemperature/mqttTemperature.ino
@@ -43,10 +43,12 @@
 // Temperature MQTT Topics
 #define MQTT_PUB_TEMP "homeassistant/dht/temperature"
 #define MQTT_PUB_HUM "homeassistant/dht/humidity"
+#define MQTT_PUB_LIGHT "homeassistant/dht/light"
 
 // Digital pin connected to the DHT sensor
 #define DHTPIN 14  
 #define LEDPIN 3
+#define LIGHTSENSOR A0
 // Uncomment whatever DHT sensor type you're using
 #define DHTTYPE DHT11   // DHT 11
 //#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
@@ -58,11 +60,19 @@ DHT dht(DHTPIN, DHTTYPE);
 // Variables to hold sensor readings
 float temp;
 float hum;
+int light; 
+float light_avg;
 float temp_old;
+float temp_avg;
+float hum_avg;
 float hum_old;
+float light_old;
+float light_sent;
 float temp_sent;
 float hum_sent;
 bool intended_disconnect=false;
+int counter_light_avg = 0;
+int counter_dht_avg = 0;
 int counter_disconnect=0;
 int counter_connect=5;
 AsyncMqttClient mqttClient;
@@ -72,10 +82,14 @@ WiFiEventHandler wifiConnectHandler;
 WiFiEventHandler wifiDisconnectHandler;
 Ticker wifiReconnectTimer;
 
-unsigned long previousMillis = 0;   // Stores last time temperature was published
-unsigned long previousMillis_hourly =0; // stores last time an hourly update was given (send values after an hour even when measurements were constant)
-const long interval = 10000;        // Interval at which to publish sensor readings
-
+unsigned long previousMillis_dht = 0;   // Stores last time temperature was published
+unsigned long previousMillis_send=0;
+unsigned long previousMillis_forced =0; // stores last time an hourly update was given (send values after an hour even when measurements were constant)
+unsigned long previousMillis_light = 0 ;
+const long interval_dht = 10000;        // Interval at which to publish sensor readings
+const long interval_light = 2000;
+const long forced_interval = 600000; //every 10mins
+const long interval_send = 60000;
 void connectToWifi() {
   if(!intended_disconnect){
   Serial.println("Connecting to Wi-Fi...");
@@ -166,18 +180,37 @@ void loop() {
   unsigned long currentMillis = millis();
   // Every X number of seconds (interval = 10 seconds) 
   // it publishes a new MQTT message
-  if (currentMillis - previousMillis >= interval) {
-    Serial.printf("Counter Connect: %d\nCounter disconnect: %d\n",counter_connect,counter_disconnect);
+  if(currentMillis-previousMillis_light >= interval_light){
+    light = analogRead(LIGHTSENSOR)*(3300/1024);
+    light_avg=(light_avg*counter_light_avg+(float)light)/(counter_light_avg+1);
+    counter_light_avg+=1;  
+    Serial.printf("Measured light: %i, Average: %f [3.3/1024 V]\n",light,light_avg);
+    previousMillis_light=currentMillis;
+  }
+  if (currentMillis - previousMillis_dht >= interval_dht) {
+    
+    
     // Save the last time a new reading was published
-    previousMillis = currentMillis;
+    previousMillis_dht = currentMillis;
     // New DHT sensor readings
     hum = dht.readHumidity();
     // Read temperature as Celsius (the default)
     temp = dht.readTemperature();
-    Serial.printf("Measured temp: %f, temp_old: %f, temp_sent: %f \nMeasured humidity: %f, hum_old: %f, hum_sent: %f\n",temp,temp_old,temp_sent,hum,hum_old,hum_sent);
+
+    temp_avg=(temp_avg*counter_dht_avg+temp)/(counter_dht_avg+1);
+    hum_avg=(hum_avg*counter_dht_avg+hum)/(counter_dht_avg+1);
+    counter_dht_avg+=1;
+    
+    Serial.printf("Measured temp: %f, Average temp: %f, temp_old: %f, temp_sent: %f \nMeasured humidity: %f, Average Humidity: %f, hum_old: %f, hum_sent: %f\n",temp,temp_avg,temp_old,temp_sent,hum,hum_avg,hum_old,hum_sent);
+    
+  }
+
+  if (currentMillis-previousMillis_send >= interval_send){
+    Serial.printf("Counter Connect: %d\nCounter disconnect: %d\n",counter_connect,counter_disconnect);
     // Read temperature as Fahrenheit (isFahrenheit = true)
     //temp = dht.readTemperature(true);
-      if(fabs(temp-temp_old)<0.1 && fabs(hum-hum_old)<0.1){
+      previousMillis_send=currentMillis;
+      if(fabs(temp_avg-temp_old)<0.1 && fabs(hum_avg-hum_old)<0.1 && fabs(light_avg-light_old)<2){
         if(counter_disconnect>=5){
           
           intended_disconnect=true;
@@ -196,8 +229,9 @@ void loop() {
         if(counter_connect >= 5){          
           intended_disconnect=false;
           counter_connect=0;
-          temp_old=temp;
-          hum_old=hum;
+          temp_old=temp_avg;
+          hum_old=hum_avg;
+          light_old=light_avg;
           
           
         }
@@ -209,20 +243,26 @@ void loop() {
        
        }
       if(mqttClient.connected()){ // only attempt to send when connected
-        
+         // Publish an MQTT message on topic esp/dht/light
+        if(light_avg!=light_sent){
+        uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_LIGHT, 1, true, String(light_avg*(3300/1024)).c_str());                            
+        Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", MQTT_PUB_LIGHT, packetIdPub1);
+        Serial.printf("Message: %.2f [mV]\n", light_avg*(3300/1024));
+        light_sent=light_avg;
+        }
         // Publish an MQTT message on topic esp/dht/temperature
-        if(temp!=temp_sent){
-        uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temp).c_str());                            
+        if(temp_avg!=temp_sent){
+        uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temp_avg).c_str());                            
         Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", MQTT_PUB_TEMP, packetIdPub1);
-        Serial.printf("Message: %.2f \n", temp);
-        temp_sent=temp;
+        Serial.printf("Message: %.2f \n", temp_avg);
+        temp_sent=temp_avg;
         }
         // Publish an MQTT message on topic esp/dht/humidity
-        if(hum!=hum_sent){
-        uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(hum).c_str());                            
+        if(hum_avg!=hum_sent){
+        uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(hum_avg).c_str());                            
         Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_HUM, packetIdPub2);
-        Serial.printf("Message: %.2f \n", hum);
-        hum_sent=hum;
+        Serial.printf("Message: %.2f \n", hum_avg);
+        hum_sent=hum_avg;
         }
       }
        
@@ -233,6 +273,7 @@ void loop() {
     if(intended_disconnect && WiFi.isConnected()){
     temp_sent=200;
     hum_sent=200;
+    light_sent=0;
     Serial.println("No temperature Change. Disconnecting from wifi");
     disconnectFromWifi();
   }
@@ -240,15 +281,19 @@ void loop() {
      wifiReconnectTimer.once(2, connectToWifi);
   }
   // Send an hourly update whether or not the measurements didn't change
-    if(currentMillis-previousMillis_hourly>3.6e6){
+    if(currentMillis-previousMillis_forced>forced_interval){
       intended_disconnect=false;
       counter_connect=6;
       counter_disconnect=0;
-      previousMillis_hourly=currentMillis;
+      previousMillis_forced=currentMillis;
       temp_sent=200;
       hum_sent=200;
-      Serial.println("Sending Hourly Update...");
+      light_sent=0;
+      Serial.println("Sending forced Update...");
     }
+//    light_avg=0;
+    counter_light_avg=0;
+    counter_dht_avg=0;
   }
   else if(digitalRead(LEDPIN)){
       digitalWrite(LEDPIN,LOW);
